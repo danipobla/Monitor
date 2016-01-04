856575a5273e9f7690ec6b05b24fa5b484b61e48
Dani Fornons <fornons@gmail.com>
2016-01-04 15:02:13 +0100

    Proves de connexio a les caracterstiques

--- app/src/main/java/cat/fornons/monitor/CardiacActivity.java
+++ app/src/main/java/cat/fornons/monitor/CardiacActivity.java
@@ -1,16 +1,35 @@
 package cat.fornons.monitor;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
+import android.bluetooth.BluetoothGattService;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothProfile;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.List;
+import java.util.UUID;
 
 public class CardiacActivity extends AppCompatActivity {
     private TextView tvNom, tvAddress;
     private String mDeviceName,mDeviceAddress;
+    private BluetoothAdapter mBluetoohAdapter;
+    private BluetoothManager mBluetoohManager;
+    private BluetoothGatt mGatt = null;
     public static final String EXTRAS_DEVICE_NAME = "DEVICE_NAME";
     public static final String EXTRAS_DEVICE_ADDRESS = "DEVICE_ADDRESS";
+
+    private List<BluetoothGattCharacteristic> characteristics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +46,55 @@ public class CardiacActivity extends AppCompatActivity {
 
         tvNom.setText(mDeviceName);
         tvAddress.setText(mDeviceAddress);
+        mBluetoohManager= (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoohAdapter = mBluetoohManager.getAdapter();
+        BluetoothDevice device = mBluetoohAdapter.getRemoteDevice(mDeviceAddress);
+
+        mGatt = device.connectGatt(this, false, gattCallback);
+        mGatt.discoverServices();
     }
+    private final BluetoothGattCallback gattCallback = new BluetoothGattCallback() {
+        @Override
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+            Log.i("onConnectionStateChange", "Status: " + status);
+            switch (newState) {
+                case BluetoothProfile.STATE_CONNECTED:
+                    Log.i("gattCallback", "STATE_CONNECTED");
+                    gatt.discoverServices();
+                    break;
+                case BluetoothProfile.STATE_DISCONNECTED:
+                    Log.e("gattCallback", "STATE_DISCONNECTED");
+                    break;
+                default:
+                    Log.e("gattCallback", "STATE_OTHER");
+            }
+
+        }
+
+        @Override
+        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+
+            List<BluetoothGattService> services = gatt.getServices();
+            Log.i("onServicesDiscovered", services.toString());
+
+            gatt.readCharacteristic(services.get(4).getCharacteristics().get(0));
+
+        }
+
+        @Override
+        public void onCharacteristicRead(BluetoothGatt gatt,
+                                         BluetoothGattCharacteristic
+                                                 characteristic, int status) {
+            Log.i("onCharacteristicRead", characteristic.toString());
+            gatt.disconnect();
+        }
+
+        @Override
+        public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
+            //super.onCharacteristicChanged(gatt, characteristic);
+            byte[] data = characteristic.getValue();
+            //Log.i("onCharacteristicRead", data.toString());
+
+        }
+    };
 }
