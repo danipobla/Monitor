a20bc88e163b11a3fccc53e1ba0131eeae9ee2bb
Dani Fornons <fornons@gmail.com>
2016-01-15 08:58:33 +0100

    Arreglada posicio del text del widget Els fitxers es guarden amb la data de inici

--- app/src/main/java/cat/fornons/monitor/HRService.java
+++ app/src/main/java/cat/fornons/monitor/HRService.java
@@ -1,10 +1,6 @@
 package cat.fornons.monitor;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.app.Service;
-import android.app.TaskStackBuilder;
 import android.appwidget.AppWidgetManager;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
@@ -18,18 +14,11 @@ import android.bluetooth.BluetoothProfile;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.AsyncTask;
 import android.os.Binder;
 import android.os.Environment;
 import android.os.IBinder;
-import android.support.v4.app.NotificationCompat;
 import android.util.Log;
-import android.view.View;
 import android.widget.Toast;
-
-import org.json.JSONObject;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
@@ -56,6 +45,8 @@ public class HRService extends Service {
     OutputStreamWriter osw;
     InputStreamReader isr;
     File file;
+    SimpleDateFormat data = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss.SSS", Locale.getDefault());
+
 
     private final IBinder binder = new mBinder();
 
@@ -89,7 +80,7 @@ public class HRService extends Service {
                 File sdCard = Environment.getExternalStorageDirectory();
                 File directory = new File (sdCard.getAbsolutePath()+ "/Monitor");
                 directory.mkdirs();
-                file = new File (directory,"monitor.txt");
+                file = new File (directory,data.format(new Date())+".txt");
                 FileOutputStream fOut = new FileOutputStream(file);
                 osw = new OutputStreamWriter(fOut);
             } catch (FileNotFoundException e) {
@@ -187,7 +178,6 @@ public class HRService extends Service {
             super.onCharacteristicChanged(gatt, characteristic);
             int flag = characteristic.getProperties();
             int format;
-            SimpleDateFormat data = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss.SSS", Locale.getDefault());
 
             if ((flag & 0x01) != 0) {
                 format = BluetoothGattCharacteristic.FORMAT_UINT16;
